9a7db2d96e4a07ed923cf1c2096c3a35e52e4d68
Sahilgill24 <gursahilgill24@gmail.com>
2024-06-28 13:11:18 +0530

    vault changes

--- src/Basic_vault.sol
+++ src/Basic_vault.sol
@@ -6,9 +6,39 @@ pragma solidity ^0.8.19;
 // and then the total debt pool will be calculated
 contract Vault {
     address[] public deposits;
-    mapping(address=>uint256) public balances;
+    struct RWA_Asset {
+        uint256 gold;
+        uint256 BTC;
+        uint256 ETH;
+
+    }
+    mapping(address=>RWA_Asset) public balances;
     const price = 5;
     uint256 public totalsupply;
+    enum RWA_Asset {
+        gold,
+        BTC,
+        ETH
+    }
+    // global debt pool mei uski values fir , red stone use krke hogi
+    // depending upon the asset deposited by the person , uski values wahan pe add hongi 
+    // price covnerter fir seedha redstone ke liye , ya chainlink bhi kr skte use 
+    // vo respective token mint krdene hein fir , and unko real life value kaise denge vo, 
+    // aage staking rewards dene hein , uska code dekhna hai 
+    // noir use krke preivacy preserving transactions implement krni kii try krni hai 
+    // get_deposit_amount, har ek asset ki value btaadega he holds
+    // balance(_address)
+    // current value
+    // dynamic collateralizaton
+    // average contribution per user or new technique for using that method
+    // making this code similiar to the one used already ins ynthetix using the account details etc.
+    // deploying them properly on fraxtal L2
+    // fallbacks add krne hein '
+    // interfaces bnaane hein 
+    // proxy upgradeable bnaana hai 
+
+
+
     function deposit() public payable{
         balances[msg.sender]+=msg.value;
         totalsupply+=msg.value;
@@ -17,7 +47,8 @@ contract Vault {
         //
 
     } 
-    
+    //deposited amount in dollars aayega 
+    //balance tuple return krega (gold,BTC,ETH) is form kaa
     function get_deposit_amount(address _address) view public returns (uint256){
         for(uint256 i;i<deposits.length;i++){
             if(_address== deposits[i]){
